5a32d31fa6b7afada05750db3230bd6d1bc2fbf8
sk <lylapp@163.com>
2019-08-26 23:12:15 +0800

    1 spring基础概念 AOP IOC DI 2 xml初始一个AOP过程

---
+++ spring/src/main/java/com/aop/ProductService.java
@@ -0,0 +1,12 @@
+package com.aop;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+public class ProductService {
+
+    public void doSOmeThing() {
+        System.out.println("do some …");
+
+    }
+}
